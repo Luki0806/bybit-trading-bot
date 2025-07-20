f3026de91f13be6011f9294439899bf6c980322d
Luki0806 <lukislo1992@gmail.com>
2025-07-20 14:49:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_KEY = "lVQmtuTkR24wWm09XG"
 API_SECRET = "xpEZmOwNZouSGIcpYXrHghs7NwMyQwNrumpv"
 SYMBOL = "WIFUSDT"
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1396430612960772126/eO-1bSIssVJqkScx0gZmvWeVgiV6PjDYlx8--cH9KkgvKX878WQc5aKJuan8eGYEidYT"
 TESTNET = False
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1396430612960772126/eO-1bSIssVJqkScx0gZmvWeVgiV6PjDYlx8--cH9KkgvKX878WQc5aKJuan8eGYEidYT"
