654fb2571763d3615d64ace06ad38ba770077427
Luki0806 <lukislo1992@gmail.com>
2025-07-24 07:03:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def calculate_qty(symbol):
         balance_info = balance_data["result"]["list"][0]["coin"]
         usdt = next(c for c in balance_info if c["coin"] == "USDT")
         available_usdt = float(usdt.get("walletBalance", 0))
-        trade_usdt = available_usdt * 0.9  # Używamy 90% dostępnego USDT
+        trade_usdt = available_usdt * 0.99  # Używamy 99% dostępnego USDT
 
         tickers_data = session.get_tickers(category="linear")
         price_info = next((item for item in tickers_data["result"]["list"] if item["symbol"] == symbol), None)
