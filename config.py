6864574206e92c9eae7a1ecebd0d2bbdc67bd39e
Luki0806 <lukislo1992@gmail.com>
2025-08-21 14:07:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_KEY = "lVQmtuTkR24wWm09XG"
 API_SECRET = "xpEZmOwNZouSGIcpYXrHghs7NwMyQwNrumpv"
-SYMBOL = "WIFUSDT"
+SYMBOL = "WIFUSDT.P"
 TESTNET = False
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1396430612960772126/eO-1bSIssVJqkScx0gZmvWeVgiV6PjDYlx8--cH9KkgvKX878WQc5aKJuan8eGYEidYT"
 POSITION_PERCENT = 0.99  # Użyj 99% salda portfela
