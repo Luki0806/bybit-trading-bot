fc8a7895c03dc6dd633191ce949ef1b91121261f
Luki0806 <lukislo1992@gmail.com>
2025-08-27 04:28:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ API_SECRET = "xpEZmOwNZouSGIcpYXrHghs7NwMyQwNrumpv"
 SYMBOL = "WIFUSDT"
 TESTNET = False
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1396430612960772126/eO-1bSIssVJqkScx0gZmvWeVgiV6PjDYlx8--cH9KkgvKX878WQc5aKJuan8eGYEidYT"
-POSITION_PERCENT = 0.5  # Użyj 50% salda portfel
+POSITION_PERCENT = 1.0  # Użyj 100% salda portfel
