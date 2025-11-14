9da12b6a6935f93dd035651d78c32736e2493463
Luki0806 <lukislo1992@gmail.com>
2025-11-14 19:11:16 +0100

    Refactor trading mode comments and remove unused lines

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ API_SECRET = "qSWZc6ca2IeOjoQQDz82EZQCvMAePkIpPQvV"
 # ⚙️ PARAMETRY BOTA
 # ======================
 SYMBOL = "WIFUSDT"  # Domyślny symbol
-ALLOWED_SYMBOLS = ["WIFUSDT", "COAIUSDT", "ZECUSDT","ZKUSDT","NEARUSDT","STRKUSDT","TRUMPUSDT"]  # Lista dozwolonych symboli
-TESTNET = False  # False = konto realne, True = testnet
+ALLOWED_SYMBOLS = ["WIFUSDT", "COAIUSDT", "ZECUSDT","ZKUSDT","NEARUSDT","STRKUSDT","TRUMPUSDT"]
+TESTNET = False
 
 # ======================
 # 💬 POWIADOMIENIA
@@ -17,13 +17,6 @@ TESTNET = False  # False = konto realne, True = testnet
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1396430612960772126/eO-1bSIssVJqkScx0gZmvWeVgiV6PjDYlx8--cH9KkgvKX878WQc5aKJuan8eGYEidYT"
 
 # ======================
-# 💰 DOMYŚLNY TRYB HANDLU
+# 💰 TRYB HANDLU (TYLKO PROCENT)
 # ======================
-# Bot używa tych wartości tylko wtedy,
-# jeśli strategia NIE przekaże "mode" i "value" w webhooku.
-POSITION_MODE = "PERCENT"   # "PERCENT" lub "SIZE"
-POSITION_VALUE = 1.0        # 1.0 = 100% kapitału lub np. 100 = 100 sztuk w trybie SIZE
-
-LEVERAGE = 5           # dźwignia dla kontraktów linear
-AUTOSCALE_QTY = True   # automatycznie zmniejsz ilość, gdy brakuje marginu
-SAFETY_MARGIN = 0.19   # nie używaj 19% dostępnego marginu
+POSITION_VALUE = 1.0   # 1.0 = 100% dostępnego kapitału
